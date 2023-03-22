1bb7f18a20e344d735228b772886c41d4dd30d4d
Mohammad Monir Hossain <monirhossain0908@gmail.com>
2023-03-22 17:18:10 +0600

    user password change - 422 status set

--- user/views.py
+++ user/views.py
@@ -222,7 +222,7 @@ class PasswordChangeAPIView(UpdateAPIView):
             })
 
         if not check_password(old_password, user.password):
-            return JsonResponse(status=status.HTTP_200_OK, data={    
+            return JsonResponse(status=status.HTTP_422_UNPROCESSABLE_ENTITY, data={    
                 "state": "warning",
                 "message": "Invalid old password.",
             })
